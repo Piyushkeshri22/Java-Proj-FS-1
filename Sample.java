0a66eed436e01e113b3d94be6a5ba7b969648237
Piyushkeshri22 <162273786+Piyushkeshri22@users.noreply.github.com>
2026-07-14 22:53:47 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,6 +4,6 @@ class Sample
     {
       System.out.println("Welcome to Jenkins");
       System.out.println("Hello");      
-      System.out.println("Jenkins Triggering Build Demo");     
+      System.out.println("Jenkins Triggering Build Automatically whenever their is changes");  
     }
   }
